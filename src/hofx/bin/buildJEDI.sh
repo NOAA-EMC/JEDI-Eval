6b4035a673b5c17d35282931b960db51b7561f1e
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2021-06-15 15:18:27 +0000

    Changes to buildJEDI script and modulefile to start working on Hera

--- src/hofx/bin/buildJEDI.sh
+++ src/hofx/bin/buildJEDI.sh
@@ -10,6 +10,11 @@
 # - run make update (if needed)
 # - run make
 # - submit/run ctests
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 /path/to/user.yaml /path/to/repos.yaml" >&2
+  exit 1
+fi
 set -eux
 
 #---- get command line arguments
@@ -22,10 +27,10 @@ src_yaml=$gitdir/bin/source_yaml
 gen_bundle=$gitdir/bin/create_bundle
 
 #---- get machine and setup build environment
-set +x
+#set +x
 machine='hera' # placeholder
 source $gitdir/cfg/platform/$machine/buildJEDI
-set -x
+#set -x
 
 #---- source needed shell variables from user YAML
 eval $($src_yaml $USERYAML user account build_dir bundle_dir clean_build clean_bundle update_jedi test_jedi)
@@ -39,13 +44,19 @@ else
   fi
 fi
 
+if [ $clean_build == "YES" ]; then
+  rm -rf $build_dir
+fi
+
 if [ ! -d $build_dir ]; then
   mkdir -p $build_dir
-else
-  if [ $clean_build == "YES" ]; then
-    rm -rf $build_dir
-  fi
 fi
+
+#----- TEMPORARY
+set +u
+export PYTHONPATH=$PYTHONPATH:$gitdir/../
+set -u
+#----- END TEMPORARY
 
 #---- create ecbuild CMakeLists.txt file
 $gen_bundle $REPOYAML $bundle_dir
