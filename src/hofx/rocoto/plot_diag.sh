ecd97bd5c5199b0e7879f7ea3d323764c87d6711
Cory Martin <cory.r.martin@noaa.gov>
2021-06-16 11:39:26 -0400

    Add placeholder scripts for arch, concat, plot diags

---
+++ src/hofx/rocoto/plot_diag.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# plot_diag.sh
+# plot/compute diagnostics for this cycle
+set -eux
+
+#----- other variables
+# REMOVE later
+MYPATH=`readlink -f "$0"`
+MYDIR=`dirname "$MYPATH"`
+gitdir=$MYDIR/..
+alias source_yaml=$gitdir/bin/source_yaml
+alias create_bundle=$gitdir/bin/create_bundle
+alias detect_host=$gitdir/bin/detect_host
+shopt -s expand_aliases
+
+#----- get machine and source runtime environment
+set +x
+machine=$(detect_host)
+source $gitdir/cfg/platform/$machine/JEDI
+set -x
+
+#----- TEMPORARY
+set +u
+export PYTHONPATH=$PYTHONPATH:$gitdir/../
+script_dir=../../../darth/hofx/ # temporary for testing
+set -u
+#----- END TEMPORARY
+
+exit 0 # this will do nothing for now
