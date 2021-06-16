09e713dd02a0777de88c25f2efde6e0a9e1bc0a1
Cory Martin <cory.r.martin@noaa.gov>
2021-06-16 11:26:19 -0400

    bugfix for tools; add initial hofx rocoto

--- src/hofx/rocoto/stage.sh
+++ src/hofx/rocoto/stage.sh
@@ -3,7 +3,7 @@
 # stage hofx files
 set -eux
 
-#---- other variables
+#----- other variables
 # REMOVE later
 MYPATH=`readlink -f "$0"`
 MYDIR=`dirname "$MYPATH"`
@@ -15,7 +15,7 @@ shopt -s expand_aliases
 
 script_dir=$gitdir # what should this variable be? coming from rocoto I assume, and not "$HOMEgfs"
 
-#---- get machine and source runtime environment
+#----- get machine and source runtime environment
 set +x
 machine=$(detect_host)
 source $gitdir/cfg/platform/$machine/JEDI
@@ -33,10 +33,6 @@ export R2D2_CONFIG=$r2d2_config
 
 #----- stage FV3JEDI files (do this here or as part of experiment setup?)
 python $script_dir/stage_fv3jedi.py $EXPDIR
-
-# here we need to either set environment variables for stage_*.py or create a YAML file to put in EXPDIR
-# currently there is cycle.yaml and dates are defined but they can also use env vars
-# TODO
 
 #----- stage cycle dependent files
 python $script_dir/stage_obs.py $EXPDIR
