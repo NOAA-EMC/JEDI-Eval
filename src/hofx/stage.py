3eafcf7d0bb6865d6c743deca3c7b86e38ce295d
Cory Martin <cory.r.martin@noaa.gov>
2021-06-17 16:52:15 -0400

    Update stage.py to match config convention

--- src/hofx/stage.py
+++ src/hofx/stage.py
@@ -17,19 +17,19 @@ def background(config):
     # NOTE: will below be handled in config already?
     bkg_time = Hour(config['cycle']) - DateIncrement(config['window_length'])
     # create directory
-    mkdir(config['bkg_dir'])
+    mkdir(config['bkg']['bkg_dir'])
     # fetch the coupler file first
     fetch(
         type='fc',
         model='gfs_metadata',
-        experiment=config['bkg_exp'],
+        experiment=config['bkg']['bkg_exp'],
         date=bkg_time,
-        step=config['forecast_steps'],
+        step=config['bkg']['forecast_steps'],
         resolution=config['model_resolution'],
         user_date_format='%Y%m%d.%H%M%S',
         fc_date_rendering='analysis',
-        database=config['bkg_db'],
-        target_file=f"{config['bkg_dir']}/$(valid_date).coupler.res",
+        database=config['bkg']['bkg_db'],
+        target_file=f"{config['bkg']['bkg_dir']}/$(valid_date).coupler.res",
         #full_report  = 'yes',
         #report = f"fetch_gfs_metadata_{config['cycle']}.yaml",
     )
@@ -37,15 +37,15 @@ def background(config):
     fetch(
         type='fc',
         model='gfs',
-        experiment=config['bkg_exp'],
+        experiment=config['bkg']['bkg_exp'],
         date=bkg_time,
-        step=config['forecast_steps'],
+        step=config['bkg']['forecast_steps'],
         resolution=config['model_resolution'],
         user_date_format='%Y%m%d.%H%M%S',
         fc_date_rendering='analysis',
-        database=config['bkg_db'],
-        target_file=f"{config['bkg_dir']}/$(valid_date).$(file_type).tile$(tile).nc",
-        tile=config['bkg_tiles'],
+        database=config['bkg']['bkg_db'],
+        target_file=f"{config['bkg']['bkg_dir']}/$(valid_date).$(file_type).tile$(tile).nc",
+        tile=config['bkg']['bkg_tiles'],
         file_type=['fv_core.res', 'fv_srf_wnd.res', 'fv_tracer.res', 'phy_data', 'sfc_data'],
         #full_report  = 'yes',
         #report = f"fetch_gfs_{config['cycle']}.yaml",
@@ -58,22 +58,22 @@ def obs(config):
     input configuration 'config' dictionary
     """
     # create directory
-    mkdir(config['obs_dir'])
+    mkdir(config['obs']['obs_dir'])
     # loop through designated observations
-    for ob in config['observations']:
+    for ob in config['obs']['observations']:
         obname = ob['obs space']['name'].lower()
         outfile = ob['obs space']['obsdatain']['obsfile']
         # try to grab obs
         fetch(
             type='ob',
-            provider=config['obs_src'],
-            experiment=config['obs_dump'],
+            provider=config['obs']['obs_src'],
+            experiment=config['obs']['obs_dump'],
             date=config['window begin'],
             obs_type=obname,
             time_window=config['window length'],
             target_file=outfile,
             ignore_missing=True,
-            database=config['obs_db'],
+            database=config['obs']['obs_db'],
             #full_report  = 'yes',
             #report = f"fetch_{obname}_{config['window begin']}.yaml",
         )
@@ -82,14 +82,14 @@ def obs(config):
             satbias = ob['obs bias']['input file']
             fetch(
                 type='bc',
-                provider=config['bc_src'],
-                experiment=config['bc_dump'],
+                provider=config['obs']['bc_src'],
+                experiment=config['obs']['bc_dump'],
                 date=config['cycle'],
                 obs_type=obname,
                 target_file=satbias,
                 file_type='satbias',
                 ignore_missing=True,
-                database=config['obs_db'],
+                database=config['obs']['obs_db'],
                 #full_report  = 'yes',
                 #report = f"fetch_satbias_{obname}_{config['window begin']}.yaml",
             )
@@ -97,14 +97,14 @@ def obs(config):
             tlapse = satbias.replace('satbias.nc4', 'tlapse.txt')
             fetch(
                 type='bc',
-                provider=config['bc_src'],
-                experiment=config['bc_dump'],
+                provider=config['obs']['bc_src'],
+                experiment=config['obs']['bc_dump'],
                 date=config['cycle'],
                 obs_type=obname,
                 target_file=tlapse,
                 file_type='tlapse',
                 ignore_missing=True,
-                database=config['obs_db'],
+                database=config['obs']['obs_db'],
                 #full_report  = 'yes',
                 #report = f"fetch_tlapse_{obname}_{config['window begin']}.yaml",
             )
@@ -117,7 +117,7 @@ def fv3jedi(config):
     uses input config dictionary for paths
     """
     # create output directory
-    mkdir(config['stage_dir'])
+    mkdir(config['fv3jedi']['stage_dir'])
     # call solo.Stage
-    path = os.path.dirname(config['fv3jedi_stage'])
-    stage = Stage(path, config['stage_dir'], config['fv3jedi_stage_files'])
+    path = os.path.dirname(config['fv3jedi']['fv3jedi_stage'])
+    stage = Stage(path, config['fv3jedi']['stage_dir'], config['fv3jedi']['fv3jedi_stage_files'])
