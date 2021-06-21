3ad052a04a841821764d7ef85433a3104a228066
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-06-21 10:32:33 -0400

    add a working window class and methods to get information from it

--- src/hofx/tools.py
+++ src/hofx/tools.py
@@ -1,6 +1,6 @@
 import platform
 from solo.template import Template, TemplateConstants
-from solo.date import JediDate, DateIncrement
+from solo.date import JediDate, DateIncrement, Minute
 
 __all__ = ['detect_host', 'process_environment_variables', 'replace_vars', 'Window']
 
@@ -38,39 +38,50 @@ def replace_vars(config):
 class Window:
 
     def __init__(self, config):
-        self.length = DateIncrement(config.get('window_length', 'PT6H')) # length of the assimilation window (beginning to end)
-        self.offset = DateIncrement(config.get('window_offset', 'PT3H')) # offset from end of the window to "analysis time"
-        self.cycle = DateIncrement(config.get('step_cycle', 'PT6H')) # Cadence
-        self.type = config.get('window_type', '3d') # 3D or 4D windows
-        self.bg_freq= DateIncrement(config.get('bg_frequency', self.cycle)) # Frequency of backgrounds
-        self.bg_step= DateIncrement(config.get('bg_step', self.cycle)) # Cadence
+        self.window_length = DateIncrement(config.get('window_length', 'PT6H')) # length of the assimilation window (beginning to end)
+        self.window_offset = DateIncrement(config.get('window_offset', 'PT3H')) # offset from end of the window to "analysis time"
+        self.step_cycle = DateIncrement(config.get('step_cycle', 'PT6H')) # Cadence
+        self.window_type = config.get('window_type', '3d') # 3D or 4D windows
+        self.bg_frequency= DateIncrement(config.get('bg_frequency', self.step_cycle)) # Frequency of backgrounds
+        self.bg_step= DateIncrement(config.get('bg_step', self.step_cycle)) # Cadence
+
+    @property
+    def info(self):
+        print('Information about the Window object:')
+        print(f'\twindow_length = {self.window_length}')
+        print(f'\twindow_offset = {self.window_offset}')
+        print(f'\twindow_type   = {self.window_type}')
+        print(f'\tstep_cycle    = {self.step_cycle}')
+        print(f'\tbg_frequency  = {self.bg_frequency}')
+        print(f'\tbg_step       = {self.bg_step}')
+        return
 
     @staticmethod
     def analysis_time(current_date):
         return current_date
 
     def window_end(self, current_date):
-        return str(JediDate(current_date) + self.offset)
+        return str(JediDate(current_date) + self.window_offset)
 
     def window_begin(self, current_date):
-        return str(JediDate(current_date) + self.offset - self.length)
+        return str(JediDate(current_date) + self.window_offset - self.window_length)
 
     # JEDI calls previous_cycle as background_time
     def background_time(self, current_date):
         return str(JediDate(current_date) - self.bg_step)
 
     def background_steps(self, current_date):
-        if self.type is '3d':
-            return [str(self.cycle)]
+        if self.window_type.lower() is '3d':
+            return [str(self.step_cycle)]
 
         window_begin = Minute(self.window_begin(current_date))
         background_time = Minute(self.background_time(current_date))
         # calculate the initial step in hours
         initial_step = DateIncrement(seconds=(window_begin - background_time) * 60)
         steps = [str(initial_step)]
-        frequency = DateIncrement(self._config['bg_frequency'])
+        frequency = DateIncrement(self.bg_frequency)
         date = window_begin + frequency
-        end = window_begin + DateIncrement(self._config['window_length'])
+        end = window_begin + DateIncrement(self.window_length)
         if end - date >= frequency.total_seconds() / 60:
             while date <= end:
                 steps.append(str(DateIncrement(seconds=(date - background_time) * 60)))
