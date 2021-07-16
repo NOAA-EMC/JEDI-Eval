92f9b2ad8daaa54efef0f7ccc461cf6f40e66dca
russ.treadon <Russ.Treadon@noaa.gov>
2021-07-16 23:09:47 +0000

    Issue #20:  correct typo in name of scripts executed by rocoto jobs

--- src/hofx/rocoto/gdas_archive.sh
+++ src/hofx/rocoto/gdas_archive.sh
@@ -4,6 +4,6 @@ set -eux
 
 export PYTHONPATH=$HOFX_HOMEDIR
 
-$HOFX_HOMEDIR/hofx/scripts/archive_example.sh $HOFX_HOMEDIR/hofx/cfg/expdir $ROTDIR/hofx_tmp
+$HOFX_HOMEDIR/hofx/scripts/archive.sh $HOFX_HOMEDIR/hofx/cfg/expdir $ROTDIR/hofx_tmp
 rc=$?
 exit $rc
