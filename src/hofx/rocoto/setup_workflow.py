273c43d5164dccd35d553ec7dbe1bb4a75c9ff47
russ.treadon <Russ.Treadon@noaa.gov>
2021-07-16 15:16:51 +0000

    Issue #20:   pass RUN_ENVIR to jobs in xml template; add comments and function to xml generator

--- src/hofx/rocoto/setup_workflow.py
+++ src/hofx/rocoto/setup_workflow.py
@@ -4,6 +4,7 @@ import sys
 import yaml
 import platform
 
+# local function to identify machine
 def detect_host():
     system = platform.system().lower()
     node = platform.node().lower()
@@ -17,41 +18,46 @@ def detect_host():
     else:
         return None
 
+# local function to return search_key value from a search_list
+def set_key(search_key,search_list):
+    res = [val for key, val in search_list.items() if search_key in key]
+    key_value=" "
+    str1 = " "
+    key_value=str1.join(res)
+    return key_value
+
+
+
+# Main body of script
+#
+# Open and read user configured experiment.yaml file
 with open("../cfg/expdir/experiment.yaml") as expyml:
    parsed_expyml=yaml.safe_load(expyml)
 
+
+# Detect machine
 machine=detect_host()
 
+
+# Extract values from user configure yaml
+#   note:  add '00' for seconds to begin and end dates
 search_key='expname'
-res = [val for key, val in parsed_expyml.items() if search_key in key]
-expname=" "
-str1 = " "
-expname=str1.join(res)
+expname=set_key(search_key,parsed_expyml)
 
 search_key='begdate'
-res = [val for key, val in parsed_expyml.items() if search_key in key]
-begdate=" "
-str1 = " "
-begdate=str1.join(res) + '00'
+begdate=set_key(search_key,parsed_expyml) + '00'
 
 search_key='enddate'
-res = [val for key, val in parsed_expyml.items() if search_key in key]
-enddate=" "
-str1 = " "
-enddate=str1.join(res) + '00'
+enddate=set_key(search_key,parsed_expyml) + '00'
 
 search_key='hofx_homedir'
-res = [val for key, val in parsed_expyml.items() if search_key in key]
-hofx_homedir=" "
-str1 = " "
-hofx_homedir=str1.join(res)
+hofx_homedir=set_key(search_key,parsed_expyml)
 
 search_key='wrkdir'
-res = [val for key, val in parsed_expyml.items() if search_key in key]
-wrkdir=" "
-str1 = " "
-wrkdir=str1.join(res)
+wrkdir=set_key(search_key,parsed_expyml)
 
+
+# Load extracte values into list.  Echo values to stdout
 replacements = {
     'expname': expname,
     'begdate': begdate,
@@ -61,17 +67,19 @@ replacements = {
     'platform': machine,
 }
 
-
 print(' ')
 print("Extract following settings")
 for src, target in replacements.items():
    print(src, target)
 
 
+# Create filename for workflow xml
 expxml=expname + '.xml'
 print(' ')
 print('Create xml: {}'.format(expxml))
 
+
+# Write workflow xml
 with open("hofx.xml", 'r') as xmlin:
   with open(expxml, 'w') as xmlout:
     for line in xmlin:
