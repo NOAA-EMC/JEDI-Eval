0ae143108d25e2a525ceb6953ecab934fbf848bd
russ.treadon <Russ.Treadon@noaa.gov>
2021-09-01 16:28:31 +0000

    Issue #37:  initial commit for clean up option

--- src/hofx/rocoto/gdas_diags.sh
+++ src/hofx/rocoto/gdas_diags.sh
@@ -4,6 +4,16 @@ set -eux
 
 export PYTHONPATH=$HOFX_HOMEDIR:$EMCPY_HOMEDIR
 
-$HOFX_HOMEDIR/hofx/scripts/diags.sh $HOFX_HOMEDIR/hofx/cfg/expdir $ROTDIR/hofx_tmp
+$HOFX_HOMEDIR/hofx/scripts/diags.sh $HOFX_HOMEDIR/hofx/cfg/expdir $ROTDIR/hofx_tmp/$CDATE
 rc=$?
+
+# If requested, remove files in $ROTDIR/diags for current cycle
+KEEPDATA=$(echo ${KEEPDATA:-"NO"} | tr a-z A-Z)
+if [[ $KEEPDATA = "NO" ]]; then
+    string=`grep "window begin" $ROTDIR/hofx_tmp/$CDATE/diags.yaml | cut -d "'" -f2-2 | head -1`
+    RMFILES=$ROTDIR/diags/*${string}.nc4
+    rm -rf $RMFILES
+#   rm -r $ROTDIR/hofx_tmp/$CDATE
+fi
+
 exit $rc
