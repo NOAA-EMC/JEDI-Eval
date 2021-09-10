29c4731c7cd26851e0d67ed2186fcf0439e0d519
russ.treadon <Russ.Treadon@noaa.gov>
2021-09-10 18:55:24 +0000

    Issue #37:  move setting runtime environment from scripts to rocoto jobs for archive and diags

--- src/hofx/rocoto/gdas_archive.sh
+++ src/hofx/rocoto/gdas_archive.sh
@@ -4,19 +4,26 @@ set -eux
 
 export PYTHONPATH=$HOFX_HOMEDIR
 
+#---- other variables
+alias source_yaml=$HOFX_HOMEDIR/hofx/bin/source_yaml
+alias create_bundle=$HOFX_HOMEDIR/hofx/bin/create_bundle
+alias detect_host=$HOFX_HOMEDIR/hofx/bin/detect_host
+shopt -s expand_aliases
+
+#---- get machine and setup runtime environment
+set +eux
+machine=${machine:-$(detect_host)}
+source $HOFX_HOMEDIR/hofx/cfg/platform/$machine/JEDI
+export R2D2_CONFIG=$HOFX_HOMEDIR/hofx/cfg/platform/$machine/r2d2_config.yaml
+set -eux
+
+#---- execute archive
 $HOFX_HOMEDIR/hofx/scripts/archive.sh $HOFX_HOMEDIR/hofx/cfg/expdir $ROTDIR/hofx_tmp/$CDATE
 rc=$?
 
-# If requested, remove stage directory for current cycle 
+#---- if requested, remove stage directory for current cycle 
 KEEPDATA=$(echo ${KEEPDATA:-"NO"} | tr a-z A-Z)
 if [[ $KEEPDATA = "NO" ]]; then
-    alias source_yaml=$HOFX_HOMEDIR/hofx/bin/source_yaml
-    shopt -s expand_aliases
-
-    set +eux
-    source $HOFX_HOMEDIR/hofx/cfg/platform/$machine/JEDI
-    set -eux
-
     eval $(source_yaml ${ROTDIR}/hofx_tmp/${CDATE}/archive.yaml archive cycle )
     DATAROOT=$ROTDIR/${cycle}
     [[ -d $DATAROOT ]] && rm -rf $DATAROOT
--- src/hofx/rocoto/gdas_diags.sh
+++ src/hofx/rocoto/gdas_diags.sh
@@ -4,19 +4,26 @@ set -eux
 
 export PYTHONPATH=$HOFX_HOMEDIR:$EMCPY_HOMEDIR
 
+#---- other variables
+alias source_yaml=$HOFX_HOMEDIR/hofx/bin/source_yaml
+alias create_bundle=$HOFX_HOMEDIR/hofx/bin/create_bundle
+alias detect_host=$HOFX_HOMEDIR/hofx/bin/detect_host
+shopt -s expand_aliases
+
+#---- get machine and setup runtime environment
+set +eux
+machine=${machine:-$(detect_host)}
+source $HOFX_HOMEDIR/hofx/cfg/platform/$machine/JEDI
+export R2D2_CONFIG=$HOFX_HOMEDIR/hofx/cfg/platform/$machine/r2d2_config.yaml
+set -eux
+
+#---- execute diags
 $HOFX_HOMEDIR/hofx/scripts/diags.sh $HOFX_HOMEDIR/hofx/cfg/expdir $ROTDIR/hofx_tmp/$CDATE
 rc=$?
 
-# If requested, remove files in $ROTDIR/diags for current cycle
+#---- if requested, remove files in $ROTDIR/diags for current cycle
 KEEPDATA=$(echo ${KEEPDATA:-"NO"} | tr a-z A-Z)
 if [[ $KEEPDATA = "NO" ]]; then
-    alias source_yaml=$HOFX_HOMEDIR/hofx/bin/source_yaml
-    shopt -s expand_aliases
-
-    set +eux
-    source $HOFX_HOMEDIR/hofx/cfg/platform/$machine/JEDI
-    set -eux
-
     eval $(source_yaml $ROTDIR/hofx_tmp/$CDATE/diags.yaml plot "window begin")
     RMFILES=$ROTDIR/diags/*${window_begin}.nc4
     rm -rf $RMFILES
