ea31df437c0a4013484797b04f706e9ffcae652c
russ.treadon <Russ.Treadon@noaa.gov>
2021-09-13 18:24:54 +0000

    Issue #37:  source single file to set up runtime environment for rocoto jobs

--- src/hofx/rocoto/gdas_hofx.sh
+++ src/hofx/rocoto/gdas_hofx.sh
@@ -2,21 +2,8 @@
 
 set -eux
 
-export PYTHONPATH=$HOFX_HOMEDIR
-
-#---- other variables
-alias source_yaml=$HOFX_HOMEDIR/hofx/bin/source_yaml
-alias create_bundle=$HOFX_HOMEDIR/hofx/bin/create_bundle
-alias detect_host=$HOFX_HOMEDIR/hofx/bin/detect_host
-shopt -s expand_aliases
-
-#---- get machine and setup runtime environment
-set +eux
-machine=${machine:-$(detect_host)}
-source $HOFX_HOMEDIR/hofx/cfg/platform/$machine/JEDI
-export R2D2_CONFIG=$HOFX_HOMEDIR/hofx/cfg/platform/$machine/r2d2_config.yaml
-set -eux
-eval $(source_yaml $HOFX_HOMEDIR/hofx/cfg/expdir/experiment.yaml account)
+#---- setup runtime evironment
+source $HOFX_HOMEDIR/hofx/cfg/setup
 
 #---- setup variables based on scheduler
 RUN_ENVIR=${RUN_ENVIR:-""}
--- src/hofx/rocoto/gdas_stage.sh
+++ src/hofx/rocoto/gdas_stage.sh
@@ -2,20 +2,8 @@
 
 set -eux
 
-export PYTHONPATH=$HOFX_HOMEDIR
-
-#---- other variables
-alias source_yaml=$HOFX_HOMEDIR/hofx/bin/source_yaml
-alias create_bundle=$HOFX_HOMEDIR/hofx/bin/create_bundle
-alias detect_host=$HOFX_HOMEDIR/hofx/bin/detect_host
-shopt -s expand_aliases
-
-#---- get machine and setup runtime environment
-set +eux
-machine=${machine:-$(detect_host)}
-source $HOFX_HOMEDIR/hofx/cfg/platform/$machine/JEDI
-export R2D2_CONFIG=$HOFX_HOMEDIR/hofx/cfg/platform/$machine/r2d2_config.yaml
-set -eux
+#---- setup runtime evironment
+source $HOFX_HOMEDIR/hofx/cfg/setup
 
 #---- execute stage
 $HOFX_HOMEDIR/hofx/scripts/stage.sh $HOFX_HOMEDIR/hofx/cfg/expdir $ROTDIR/hofx_tmp/$CDATE
